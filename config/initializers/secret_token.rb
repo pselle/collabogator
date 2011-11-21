25c9c29b914cfc8b90dcb7201e074786319d61d9
Pam Selle <pamela.selle@gmail.com>
2011-11-21 12:54:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Collabogator::Application.config.secret_token = 'b897bbdfe1209c754487c14509e2d9dad7333b59a17303b2fba1c321630e62cb52909fd6c9eb73d5141e917821512785335c66504dfeaa6af1d407f2588c8617'
