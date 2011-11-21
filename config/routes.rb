d96a0efc9b2a3c16bfa0f04a16475cda0c012db7
Pam Selle <pamela.selle@gmail.com>
2011-11-21 13:24:09 -0500

    Created topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Collabogator::Application.routes.draw do
+  resources :topics
+
   resources :users
 
   # The priority is based upon order of creation:
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111121175610) do
+ActiveRecord::Schema.define(:version => 20111121182320) do
+
+  create_table "topics", :force => true do |t|
+    t.string   "title"
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
